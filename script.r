287c814034216f1301ae8d95a73b0e871c72d61c
angelopol <114349306+angelopol@users.noreply.github.com>
2024-04-30 09:58:18 -0400

    R basic sintax

---
+++ script.r
@@ -0,0 +1,14 @@
+#assign value to variable
+x <- 5
+y <- 10
+#operate variables
+x + y
+x - y
+x / y
+x * y
+x %% y #modulo
+x ^ y
+#variable types
+z <- "hi" #string
+v <- TRUE #boolean
+p <- 12.343 #numeric
